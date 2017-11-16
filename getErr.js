2514fb1653b2acbbd5c263c28ee0028d41a39a3e
zhangzju <zhangway19921221@163.com>
2017-11-16 20:44:22 +0800

    正确引入lodash

--- getErr.js
+++ getErr.js
@@ -1,5 +1,11 @@
 var _ = require('lodash');
 var $ = require('jquery');
+var xlsx = require('node-xlsx');
 var err = require('./err');
 
-console.log(err)
+var data = [];
+_.forIn(err, function(value, key) {
+  // console.log([key,value]);
+  _.concat(data,[key,value]);
+  console.log(data);
+})
